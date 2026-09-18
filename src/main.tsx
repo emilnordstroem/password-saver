9ec1be9eba73256b68ebce3f1db87816d33f416a
Emil Nordstroem <emilstoeve@outlook.com>
2026-09-18 20:50:25 +0200

    Basic empty react application

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { NextUIProvider } from '@nextui-org/react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+createRoot(document.getElementById('root')!).render(
+  <NextUIProvider>
+    <App />
+  </NextUIProvider>,
+);
