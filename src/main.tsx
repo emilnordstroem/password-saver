2a9e10e51f26313a2c6b3d5d59659b8d89e97103
Emil Nordstroem <emilstoeve@outlook.com>
2026-09-20 10:21:41 +0200

    redux with search

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
+import { store } from "./store";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <NextUIProvider>
-    <App />
-  </NextUIProvider>,
+  <Provider store={store}>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </Provider>,
 );
